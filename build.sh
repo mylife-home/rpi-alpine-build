3924131b18a6916cbfef710f545477204dcb7e3b
vincent-tr <vincent.tr@gmail.com>
2021-06-06 18:37:37 +0200

    wip build script

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+#################################
+# BASE
+#################################
+
 export ALPINE_VERSION=3.13.5
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+#################################
+# aarch64
+#################################
+
 # export LINUX_PLATFORM=aarch64 => unmae -m in container
 export ALPINE_IMAGE_PLATFORM=arm64v8
 export DOCKER_PLATFORM=arm64
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $(realpath ./build):/build $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /build/container-build.sh
