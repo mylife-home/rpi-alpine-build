9674e916f29db5095c32151f3a0907e7a6d7d163
vincent-tr <vincent.tr@gmail.com>
2021-06-07 21:20:24 +0200

    wip abuild

--- build/container-build.sh
+++ build/container-build.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-uname -m
+# setup user + abuild
+
+apk add --no-cache sudo git alpine-sdk
+
+adduser -D builder
+echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+addgroup builder abuild
+mkdir -p /var/cache/distfiles
+chmod a+w /var/cache/distfiles
+
+#######
