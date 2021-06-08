a57fb6b78e0b1b98461c8f24440be2a744708a5c
vincent-tr <vincent.tr@gmail.com>
2021-06-08 21:13:31 +0200

    mount secrets

--- build.sh
+++ build.sh
@@ -16,4 +16,4 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 export ALPINE_IMAGE_PLATFORM=arm64v8
 export DOCKER_PLATFORM=arm64
 
-docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $(realpath ./build):/build $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /build/container-build.sh
+docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $(realpath ./build):/build -v $(realpath ../mylife-home-studio-data-dev/build-secrets):/build-secrets $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /build/container-build.sh
