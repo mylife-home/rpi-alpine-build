3c492d9f03c5cf5a47f6d771e519120c6a7e7a81
vincent-tr <vincent.tr@gmail.com>
2021-06-08 22:52:18 +0200

    move stuff

--- build.sh
+++ build.sh
@@ -19,4 +19,4 @@ export DOCKER_PLATFORM=arm64
 SECRETS_PATH=../mylife-home-studio-data-dev/build-secrets
 DIST_PATH=../mylife-home-main/packages/mylife-home-packager/dist
 
-docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $(realpath ./build):/mnt/build -v $(realpath $SECRETS_PATH):/mnt/build-secrets -v $(realpath $DIST_PATH):/mnt/dist $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /mnt/build/container-build.sh
+docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $(realpath ./scripts):/mnt/scripts -v $(realpath ./build):/mnt/build -v $(realpath $SECRETS_PATH):/mnt/build-secrets -v $(realpath $DIST_PATH):/mnt/dist $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /mnt/scripts/container-build.sh
