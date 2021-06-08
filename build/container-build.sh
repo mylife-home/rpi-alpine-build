59ccd770d4474f3b3eb0b39375ad1dbceafc06fb
vincent-tr <vincent.tr@gmail.com>
2021-06-08 21:13:46 +0200

    prepare

--- build/container-build.sh
+++ build/container-build.sh
@@ -11,4 +11,23 @@ addgroup builder abuild
 mkdir -p /var/cache/distfiles
 chmod a+w /var/cache/distfiles
 
+home=/home/builder
+
+sudo -i -u builder /bin/sh - << eof
+
+mkdir -p $home/.ssh
+cp -r /build-secrets/ssh-keys/* $home/.ssh/
+chmod 700 $home/.ssh
+
+mkdir -p $home/.abuild
+cp /build-secrets/abuild/* $home/.abuild
+
+eof
+
 #######
+
+/bin/sh
+
+
+# su - builder
+# mkdir -p ~/packages
