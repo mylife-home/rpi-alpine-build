48046d7b5294865b1e6849dda431eaac13681e8c
vincent-tr <vincent.tr@gmail.com>
2021-08-26 11:17:14 +0200

    use subfolder

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-#################################
-# BASE
-#################################
+ALPINE_VERSION=3.13.5
+SECRETS_PATH=$(realpath ../mylife-home-studio-data-dev/build-secrets)
 
-export ALPINE_VERSION=3.13.5
+function init() {
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
+}
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
+function build_platform() {
+  local ALPINE_IMAGE_PLATFORM=$1
+  local DOCKER_PLATFORM=$2
+  local BUILD_PATH=$(realpath ./build)
 
-#################################
-# aarch64
-#################################
+  mkdir -p $BUILD_PATH/packages # to be created with right user
+  docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $BUILD_PATH/packages:/mnt/build -v $(realpath ./scripts):/mnt/scripts:ro -v $(realpath ./packages):/mnt/packages:ro -v $(realpath $SECRETS_PATH):/mnt/build-secrets:ro $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /mnt/scripts/container-build-packages.sh $(id -u) $(id -g)
+}
 
-# export LINUX_PLATFORM=aarch64 => uname -m in container
-export ALPINE_IMAGE_PLATFORM=arm64v8
-export DOCKER_PLATFORM=arm64
-
-SECRETS_PATH=../mylife-home-studio-data-dev/build-secrets
-
-mkdir -p $(realpath ./build) # to be created with right user
-docker run --platform linux/$DOCKER_PLATFORM --rm -ti -v $(realpath ./build):/mnt/build -v $(realpath ./scripts):/mnt/scripts:ro -v $(realpath ./packages):/mnt/packages:ro -v $(realpath $SECRETS_PATH):/mnt/build-secrets:ro $ALPINE_IMAGE_PLATFORM/alpine:$ALPINE_VERSION /mnt/scripts/container-build-packages.sh $(id -u) $(id -g)
+init
+build_platform arm64v8 arm64 # aarch64 (rpi2 V1.2, rpi3, rpi4)
+build_platform arm32v6 arm # armhf (rpi1, rpi0, rpi0w)
+build_platform arm32v7 arm # armv7l (rpi2 V1.1)
