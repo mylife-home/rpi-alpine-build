201014fc14671720f899d97f821baacff95d30cc
vincent-tr <vincent.tr@gmail.com>
2021-09-04 17:08:33 +0200

    bump

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-REPOSITORY_VERSION=1.0.0
+REPOSITORY_VERSION=1.0.1
 ALPINE_VERSION=3.13.5
 SECRETS_PATH=$(realpath ../mylife-home-studio-data-dev/build-secrets)
 BUILD_PATH=$(realpath ./build)
