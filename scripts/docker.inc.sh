f5f905a4cab37ce6f549d4692326c76f87717ba5
vincent-tr <vincent.tr@gmail.com>
2021-09-10 18:26:44 +0200

    order

--- scripts/docker.inc.sh
+++ scripts/docker.inc.sh
@@ -5,9 +5,9 @@ function docker_init() {
 function docker_run_platforms() {
   local SCRIPT=$1
 
-  docker_build_platform arm64v8 arm64 aarch64 $SCRIPT # (rpi2 V1.2, rpi3, rpi4)
   docker_build_platform arm32v6 arm armhf $SCRIPT # (rpi1, rpi0, rpi0w)
   docker_build_platform arm32v7 arm armv7l $SCRIPT # (rpi2 V1.1)
+  docker_build_platform arm64v8 arm64 aarch64 $SCRIPT # (rpi2 V1.2, rpi3, rpi4)
 }
 
 function docker_build_platform() {
