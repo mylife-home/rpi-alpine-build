4b940037ef8aaf0ebb71a2a54feaf4d3f1702a40
vincent-tr <vincent.tr@gmail.com>
2021-09-17 17:10:07 +0200

    driver-absoluta + bump

--- scripts/defines.inc.sh
+++ scripts/defines.inc.sh
@@ -1,4 +1,4 @@
-DOCKER_PACKAGES_REPOSITORY_VERSION=1.0.9
+DOCKER_PACKAGES_REPOSITORY_VERSION=1.0.10
 ALPINE_VERSION=3.13.5
 SECRETS_PATH=$(realpath ../mylife-home-studio-data-dev/build-secrets)
 BUILD_PATH=$(realpath ./build)
