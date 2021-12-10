2018545dac36dd4a624d01977ab0310b2f47fb6e
vincent-tr <vincent.tr@gmail.com>
2021-12-10 16:52:50 +0100

    bump

--- scripts/defines.inc.sh
+++ scripts/defines.inc.sh
@@ -1,4 +1,4 @@
-DOCKER_PACKAGES_REPOSITORY_VERSION=1.0.34
+DOCKER_PACKAGES_REPOSITORY_VERSION=1.0.35
 ALPINE_VERSION=3.13.5
 SECRETS_PATH=$(realpath ../mylife-home-studio-data-dev/build-secrets)
 BUILD_PATH=$(realpath ./build)
